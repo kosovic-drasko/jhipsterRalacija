03244a796607505c6e77b8047ce5c84f1bb74c7a
kosovic-drasko <drasko.kosovic@gmail.com>
2022-03-16 18:17:20 +0100

    Initial version of jhipsterRalacija generated by generator-jhipster@7.7.0

---
+++ src/main/java/relacija/config/ApplicationProperties.java
@@ -0,0 +1,12 @@
+package relacija.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Properties specific to Jhipster Ralacija.
+ * <p>
+ * Properties are configured in the {@code application.yml} file.
+ * See {@link tech.jhipster.config.JHipsterProperties} for a good example.
+ */
+@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+public class ApplicationProperties {}
---
+++ src/test/java/relacija/IntegrationTest.java
@@ -0,0 +1,17 @@
+package relacija;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import relacija.JhipsterRalacijaApp;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = JhipsterRalacijaApp.class)
+public @interface IntegrationTest {
+}
